2e2f53a6f78eb6ae4bec8f5f30f9ccd817350816
devashishsoni98 <devashishsoni926@gmail.com>
2023-08-28 18:01:05 +0530

    updating to a working contact form using emailjs

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,39 +1,59 @@
-
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 const Contact = () => {
-  const handleSubmit = (e) => {
+
+  const userid = process.env.REACT_APP_USER_ID;
+  const templateid = process.env.REACT_APP_TEMPLATE;
+  const myapi = process.env.REACT_APP_EMAIL_API;
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
+
+    emailjs
+      .sendForm(
+        userid,
+        templateid,
+        form.current,
+        myapi
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+          alert("Message sent successfully!");
+        },
+        (error) => {
+          alert("Something went wrong! Try again after some time.");
+        }
+      );
   };
   return (
     <>
-    <div className="contact">
-      <h1 className="c-h1">Contact Me</h1>
-      <div className="formm">
-        <form className="forum">
-          <input
-            name="name"
-            type="text"
-            class="feedback-input"
-            placeholder="Name"
-          />
-          <input
-            name="email"
-            type="text"
-            class="feedback-input"
-            placeholder="Email"
-          />
-          <textarea
-            name="text"
-            class="feedback-input"
-            placeholder="Message"
-          ></textarea>
-          <input
-            className="form-btn"
-            type="submit"
-            value="SUBMIT"
-            onClick={handleSubmit}
-          />
-        </form>
-      </div>
+      <div className="contact">
+        <h1 className="c-h1">Contact Me</h1>
+        <div className="formm">
+          <form ref={form} onSubmit={sendEmail} className="forum">
+            <input
+              type="text"
+              name="user_name"
+              className="feedback-input"
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              name="user_email"
+              className="feedback-input"
+              placeholder="Email"
+            />
+            <textarea
+              name="message"
+              className="feedback-input"
+              placeholder="Message"
+            />
+            <input type="submit" value="Send" className="form-btn" />
+          </form>
+        </div>
       </div>
     </>
   );
